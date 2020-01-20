9feea9e64d777cfd98342325c4133a08eedc26fe
BurNing1993 <786678257@qq.com>
2020-01-20 16:42:08 +0800

    Updates

---
+++ precache-manifest.782b76f77eda935c264e8b6f5f0a5bce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fcc1a9e63d1299b63ae569fec4c8668",
+    "url": "/react-antd-admin/index.html"
+  },
+  {
+    "revision": "520afb77bb3969a1c379",
+    "url": "/react-antd-admin/static/css/2.d54adac0.chunk.css"
+  },
+  {
+    "revision": "289f8749b6eff972ea98",
+    "url": "/react-antd-admin/static/css/main.a231459d.chunk.css"
+  },
+  {
+    "revision": "520afb77bb3969a1c379",
+    "url": "/react-antd-admin/static/js/2.548afa1d.chunk.js"
+  },
+  {
+    "revision": "a4555d1811ed97021f1f54c04d75541b",
+    "url": "/react-antd-admin/static/js/2.548afa1d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "289f8749b6eff972ea98",
+    "url": "/react-antd-admin/static/js/main.9b28a025.chunk.js"
+  },
+  {
+    "revision": "9277bd8e35dd884bddfc",
+    "url": "/react-antd-admin/static/js/runtime-main.0ffd9c6a.js"
+  }
+]);
