162178cbb1adc60d15598303e3e5a909b0fdc421
BurNing1993 <786678257@qq.com>
2020-02-28 14:08:34 +0800

    add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
-import React from 'react';
-import './App.css';
-import { Button } from 'antd';
-
-import {useStore} from './store'
-import {observer} from 'mobx-react'
+import React,{Suspense, lazy} from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./App.css";
+import NotFound from './components/NotFound'
+import { Spin } from 'antd';
 
 function App() {
-  const {counterStore:{count,increase}} = useStore()
   return (
-    <div className="App">
-      <span>{count}</span>
-      <Button type="primary" onClick={()=>increase()}>Increase</Button>
-    </div>
+    <BrowserRouter>
+      <div id="app">
+        <Suspense fallback={<Spin/>}>
+          <Switch>
+            <Route path="/" exact component={lazy(()=>import('./pages/home'))}  />
+            <Route path="/login"  component={lazy(()=>import('./pages/login'))}  />
+            <Route component={NotFound}/>
+          </Switch>
+        </Suspense>
+      </div>
+    </BrowserRouter>
   );
 }
 
-export default observer(App);
+export default App;
---
+++ src/components/Loading.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+
+const Loading = () => <div className="loading">loading</div>;
+export default Loading;
