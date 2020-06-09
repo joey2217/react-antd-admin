365cb0587cbc712f475476d5918fe294273a7fde
BurNing1993 <786678257@qq.com>
2020-06-09 16:37:38 +0800

    chore:up mock test

--- api/test.ts
+++ api/test.ts
@@ -2,9 +2,9 @@ import { NowRequest, NowResponse } from '@vercel/node'
 import { Random } from "mockjs";
 
 export default (request: NowRequest, response: NowResponse) => {
-  response.status(200).json({
-    request,
-    response,
+  response.status(200).send({
+    req:request,
+    res:response,
     name: Random.cname(),
     token: Random.guid(),
     message: '登录成功!'
