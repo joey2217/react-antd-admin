2ad65d9f3671b7f5cc5fb5e233d9ed09dc6ef5f5
BurNing1993 <786678257@qq.com>
2020-06-12 14:31:58 +0800

    chore:up mock Account

--- api/type.ts
+++ api/type.ts
@@ -1,1 +1,25 @@
+import { type } from "os";
+
 export const TOKEN_KEY = 'react-admin-request-token'
+
+// Account
+
+export type Role = | "admin" | "user"
+
+enum Status {
+  DISABLED,
+  ENABLED,
+}
+
+export class Account {
+  username: string;
+  account: string;
+  status: Status;
+  roles: Role[];
+  constructor(username: string, account: string, status: number, roles: Role[]) {
+    this.username = username;
+    this.account = account;
+    this.status = status;
+    this.roles = roles;
+  }
+}
