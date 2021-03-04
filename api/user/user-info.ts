c0db0034a8f9f7be53f7008847d0f5323b22e2ab
Joey <786678257@qq.com>
2021-03-04 11:17:52 +0800

    mock:

--- api/user/user-info.ts
+++ api/user/user-info.ts
@@ -2,6 +2,120 @@ import { NowRequest, NowResponse } from '@vercel/node'
 import { Random } from "mockjs";
 import { TOKEN_KEY } from '../type'
 
+interface IMenu {
+  path: string;
+  title: string;
+  icon?: any;
+  children?: Array<IMenu>;
+}
+
+const menuList: Array<IMenu> = [
+  {
+    path: "/home",
+    title: "home",
+    icon: 'DashboardOutlined',
+  },
+  {
+    path: "/table",
+    title: "table",
+    icon: 'TableOutlined',
+  },
+  {
+    path: "/form",
+    title: "form",
+    icon: 'FormOutlined',
+  },
+  {
+    path: "/system",
+    title: "system",
+    icon: 'SettingOutlined',
+    children: [
+      {
+        path: "/system/account",
+        title: "account",
+      },
+      {
+        path: "/system/auth",
+        title: "auth",
+      },
+      {
+        path: "/system/role",
+        title: "role",
+      },
+    ],
+  },
+  {
+    path: "/editor",
+    title: "editor",
+    icon: 'HighlightOutlined',
+    children: [
+      {
+        path: "/editor/flow",
+        title: "flow",
+      },
+      {
+        path: "/editor/mind",
+        title: "mind",
+      },
+    ],
+  },
+  {
+    path: "/charts",
+    title: "charts",
+    icon: 'LineChartOutlined',
+    children: [
+      {
+        path: "/charts/line",
+        title: "lineCharts",
+      },
+      {
+        path: "/charts/common",
+        title: "commonCharts",
+      },
+    ],
+  },
+  {
+    path: "/text-editor",
+    title: "textEditor",
+    icon: 'EditOutlined',
+    children: [
+      {
+        path: "/text-editor/markdown",
+        title: "markdown",
+      },
+      {
+        path: "/text-editor/rich-text",
+        title: "richText",
+      },
+    ],
+  },
+  {
+    path: "/nested-menu",
+    title: "nestedMenu",
+    icon: 'MenuOutlined',
+    children: [
+      {
+        path: "/nested-menu/menu1",
+        title: "menu1",
+        children: [
+          {
+            path: "/nested-menu/menu1/menu1-1",
+            title: "menu11",
+          },
+          {
+            path: "/nested-menu/menu1/menu1-2",
+            title: "menu12",
+          },
+        ],
+      },
+      {
+        path: "/nested-menu/menu2",
+        title: "menu2",
+      },
+    ],
+  },
+];
+
 module.exports = (req: NowRequest, res: NowResponse) => {
   const token = req.headers[TOKEN_KEY]
   if (token) {
@@ -11,7 +125,8 @@ module.exports = (req: NowRequest, res: NowResponse) => {
       userId: Random.natural(),
       message: '获取用户信息成功!',
       roles: ['admin', 'user'],
-      avatar: 'https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png'
+      avatar: 'https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png',
+      menus:menuList
     })
   } else {
     res.status(403).json({
@@ -20,5 +135,4 @@ module.exports = (req: NowRequest, res: NowResponse) => {
       headers:req.headers,
     })
   }
-
 };
