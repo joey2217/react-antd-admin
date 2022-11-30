51adbf1b77833775000f9ce18d8a999e68ad22b5
Joey <786678257@qq.com>
2022-11-30 16:09:07 +0800

    chore: i18n

--- src/pages/error/404.tsx
+++ src/pages/error/404.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
-import { Button, Result } from 'antd';
+import React from 'react'
+import { Button, Result } from 'antd'
+import { Link } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
 
-const NotFound: React.FC = () => (
-  <Result
-    status="404"
-    title="404"
-    subTitle="Sorry, the page you visited does not exist."
-    extra={<Button type="primary">Back Home</Button>}
-  />
-);
+const NotFound: React.FC = () => {
+  const { t } = useTranslation()
+  return (
+    <Result
+      status="404"
+      title="404"
+      subTitle={t('notFoundSubTitle')}
+      extra={
+        <Link to="/">
+          <Button type="primary">{t('backHome')}</Button>
+        </Link>
+      }
+    />
+  )
+}
 
-export default NotFound;
+export default NotFound
