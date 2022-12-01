46275921b7028adc1ad8afca32cdbf3ae5a7315e
Joey <786678257@qq.com>
2022-12-01 08:57:32 +0800

    feat: login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,9 @@
-import { Card, Col, Row } from 'antd'
+import { Card, Col, Row, Space } from 'antd'
 import React, { memo } from 'react'
 import { useTranslation } from 'react-i18next'
 import LoginForm from './LoginForm'
+import ThemeSwitch from '../../layout/Header/ThemeSwitch'
+import LanguageDropdown from '../../layout/Header/LanguageDropdown'
 
 const Login: React.FC = () => {
   const { t } = useTranslation()
@@ -13,7 +15,16 @@ const Login: React.FC = () => {
       className="h-screen pb-20 bg-gradient-to-r from-blue-200 to-cyan-200"
     >
       <Col xs={24} sm={16} md={14} lg={12} xl={6}>
-        <Card className="login-card" title={t('login.title')} extra={'TODO'}>
+        <Card
+          className="login-card"
+          title={t('login.title')}
+          extra={
+            <Space size="middle">
+              <ThemeSwitch />
+              <LanguageDropdown />
+            </Space>
+          }
+        >
           <LoginForm />
         </Card>
       </Col>
